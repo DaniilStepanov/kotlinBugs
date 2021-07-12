1aa28a1c199d50e322c1b9c9da7494d59de19323
DaniilStepanov <stepanov0995@mail.ru>
2021-07-12 16:15:09 +0300

    added diff compiles

---
+++ diffCompile/gjljfuu_FILE.kt
@@ -0,0 +1,23 @@
+// Bug happens on JVM , JVM -Xuse-old-backend
+// WITH_REFLECT
+// IGNORE_BACKEND: JS_IR
+// IGNORE_BACKEND: JS_IR_ES6
+// IGNORE_BACKEND: JS, NATIVE
+// FILE: tmp0.kt
+
+
+fun box()   = 
+    try {
+fun box()   =
+try {
+class Holder
+}
+catch(e: AssertionError){}
+}
+finally {
+fun box()   =
+try {}
+finally {
+ class Holder
+}
+}
---
+++ diffCompile/tggcunz_FILE.kt
@@ -0,0 +1,14 @@
+// Bug happens on JVM , JVM -Xuse-old-backend
+// WITH_RUNTIME
+// FILE: tmp0.kt
+
+
+class Kla1 {
+    @JvmField
+    val prope3 = ""
+companion object 
+ {
+        val prope3 = ""
+    }
+}
+
